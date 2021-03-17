cb33c21b352fa121a2206cb947968643b568f301
Kaleb <kaleb.loo@gmail.com>
2021-03-17 06:49:43 +0000

    Disable Chat During Player's Turn

--- client/src/components/Chat.jsx
+++ client/src/components/Chat.jsx
@@ -6,7 +6,9 @@ const Chat = ({ player, data, id, onSubmit }) => {
     const [message, setMessage] = useState('');
 
     useEffect(() => {
-        divRef.current.scrollIntoView({ behavior: 'smooth' });
+        if (divRef.current !== null) {
+            divRef.current.scrollIntoView({ behavior: 'smooth' });
+        }
     });
 
     // handle message
@@ -23,22 +25,26 @@ const Chat = ({ player, data, id, onSubmit }) => {
 
     return (
         <div>
-            <h4>Chat</h4>
-            <div className='chat-area'>
-                <ul className='chat-list'>
-                    {data.length > 0 && data.map((d) => {
-                        return <li className={player.id === d.player.id ? 'self-message' : 'team-message'}>
-                            <p className='player-name'>{d.player.name}: </p>
-                            <p className='message'>{d.message}</p>
-                        </li>
-                    })}
-                </ul>
-                <div ref={divRef}></div>
-            </div>
-            <form className='chat-input' onSubmit={submitMessage}>
-                <FocusInput type="text" value={message} onChange={handleMessage} required="required" />
-                <input className="submit" type="submit" value=">>" />
-            </form>
+            {(player.turn === false) &&
+                <>
+                    <h4>Chat</h4>
+                    <div className='chat-area'>
+                        <ul className='chat-list'>
+                            {data.length > 0 && data.map((d) => {
+                                return <li className={player.id === d.player.id ? 'self-message' : 'team-message'}>
+                                    <p className='player-name'>{d.player.name}: </p>
+                                    <p className='message'>{d.message}</p>
+                                </li>
+                            })}
+                        </ul>
+                        <div ref={divRef}></div>
+                    </div>
+                    <form className='chat-input' onSubmit={submitMessage}>
+                        <FocusInput type="text" value={message} onChange={handleMessage} required="required" />
+                        <input className="submit" type="submit" value=">>" />
+                    </form>
+                </>
+            }
         </div>
     )
 }
