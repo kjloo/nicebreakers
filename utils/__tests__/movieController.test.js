adab81f9aee66dc45a1aea5361fd36ed4f1069fc
Kaleb <kaleb.loo@gmail.com>
2021-10-10 13:14:01 -1000

    Organize Unit Test

--- utils/__tests__/movieController.test.js
+++ utils/__tests__/movieController.test.js
@@ -6,152 +6,181 @@ jest.mock('../emitter');
 
 let controller = new MovieController(undefined, stubGame);
 
-beforeEach(() => {
-    controller.resetGameState(undefined, stubGame);
-});
+describe('Movie Controller', () => {
 
-test('check current player', () => {
-    let teamIndex = 0;
-    let playerIndex = 0;
-    // Player should be first player on first team
-    expect(controller.getCurrentPlayer(stubGame)).toEqual(stubGame.teams[teamIndex].players[playerIndex]);
-    // Move to next player
-    controller.incrementPlayerIndex(stubGame);
-    playerIndex = 1;
-    // Player should be sean
-    expect(controller.getCurrentPlayer(stubGame)).toEqual(stubGame.teams[teamIndex].players[playerIndex]);
-    // Move to next player
-    controller.incrementPlayerIndex(stubGame);
-    // Should wrap back around. Player should be bob
-    playerIndex = 0;
-    expect(controller.getCurrentPlayer(stubGame)).toEqual(stubGame.teams[teamIndex].players[playerIndex]);
+    beforeEach(() => {
+        controller.resetGameState(undefined, stubGame);
+    });
 
-    // Excercise player turn change
-    controller.changePlayerTurns(stubGame);
-    teamIndex = 1;
-    expect(controller.getCurrentPlayer(stubGame)).toEqual(stubGame.teams[teamIndex].players[playerIndex]);
-    expect(stubGame.teams[0].players[0].turn).toBe(false);
-    expect(stubGame.teams[teamIndex].players[playerIndex].turn).toBe(true);
-    expect(controller.getCurrentTeam(stubGame)).toEqual(stubGame.teams[teamIndex]);
+    test('check current player', () => {
+        let teamIndex = 0;
+        let playerIndex = 0;
+        // Player should be first player on first team
+        expect(controller.getCurrentPlayer(stubGame)).toEqual(stubGame.teams[teamIndex].players[playerIndex]);
+        // Move to next player
+        controller.incrementPlayerIndex(stubGame);
+        playerIndex = 1;
+        // Player should be sean
+        expect(controller.getCurrentPlayer(stubGame)).toEqual(stubGame.teams[teamIndex].players[playerIndex]);
+        // Move to next player
+        controller.incrementPlayerIndex(stubGame);
+        // Should wrap back around. Player should be bob
+        playerIndex = 0;
+        expect(controller.getCurrentPlayer(stubGame)).toEqual(stubGame.teams[teamIndex].players[playerIndex]);
 
-    // Exercise increment game state
-    controller.incrementGameState(undefined, stubGame);
-    teamIndex = 0;
-    playerIndex = 1;
-    expect(controller.getCurrentPlayer(stubGame)).toEqual(stubGame.teams[teamIndex].players[playerIndex]);
-    expect(stubGame.teams[1].players[0].turn).toBe(false);
-    expect(stubGame.teams[teamIndex].players[playerIndex].turn).toBe(true);
-    expect(controller.getCurrentTeam(stubGame)).toEqual(stubGame.teams[teamIndex]);
-});
+        // Excercise player turn change
+        controller.changePlayerTurns(stubGame);
+        teamIndex = 1;
+        expect(controller.getCurrentPlayer(stubGame)).toEqual(stubGame.teams[teamIndex].players[playerIndex]);
+        expect(stubGame.teams[0].players[0].turn).toBe(false);
+        expect(stubGame.teams[teamIndex].players[playerIndex].turn).toBe(true);
+        expect(controller.getCurrentTeam(stubGame)).toEqual(stubGame.teams[teamIndex]);
 
-test('check current team', () => {
-    let teamIndex = 0;
-    // Team should be fish
-    expect(controller.getCurrentTeam(stubGame)).toEqual(stubGame.teams[teamIndex]);
-    // Move to next team
-    controller.incrementTeamIndex(stubGame);
-    teamIndex = 1;
-    // Team should be cat
-    expect(controller.getCurrentTeam(stubGame)).toEqual(stubGame.teams[teamIndex]);
-    // Move to next team
-    controller.incrementTeamIndex(stubGame);
-    teamIndex = 0;
-    // Should wrap back around. Team should be fish
-    expect(controller.getCurrentTeam(stubGame)).toEqual(stubGame.teams[teamIndex]);
+        // Exercise increment game state
+        controller.incrementGameState(undefined, stubGame);
+        teamIndex = 0;
+        playerIndex = 1;
+        expect(controller.getCurrentPlayer(stubGame)).toEqual(stubGame.teams[teamIndex].players[playerIndex]);
+        expect(stubGame.teams[1].players[0].turn).toBe(false);
+        expect(stubGame.teams[teamIndex].players[playerIndex].turn).toBe(true);
+        expect(controller.getCurrentTeam(stubGame)).toEqual(stubGame.teams[teamIndex]);
+    });
 
-    // Use change team turns
-    controller.changeTeamTurns(stubGame);
-    teamIndex = 1;
-    // Team should be cat
-    expect(controller.getCurrentTeam(stubGame)).toEqual(stubGame.teams[teamIndex]);
-    expect(stubGame.teams[teamIndex].turn).toBe(true);
-    expect(stubGame.teams[0].turn).toBe(false);
-});
+    test('check current team', () => {
+        let teamIndex = 0;
+        // Team should be fish
+        expect(controller.getCurrentTeam(stubGame)).toEqual(stubGame.teams[teamIndex]);
+        // Move to next team
+        controller.incrementTeamIndex(stubGame);
+        teamIndex = 1;
+        // Team should be cat
+        expect(controller.getCurrentTeam(stubGame)).toEqual(stubGame.teams[teamIndex]);
+        // Move to next team
+        controller.incrementTeamIndex(stubGame);
+        teamIndex = 0;
+        // Should wrap back around. Team should be fish
+        expect(controller.getCurrentTeam(stubGame)).toEqual(stubGame.teams[teamIndex]);
 
-test('increment game state', () => {
-    // Init state
-    expect(stubGame.answer).toBeUndefined();
-    expect(stubGame.cachedPlayers).toEqual([]);
-    // check sum of scores is 0
-    let scores = stubGame.teams.reduce((acc, cur) => acc + cur.score, 0);
-    expect(scores).toBe(0);
+        // Use change team turns
+        controller.changeTeamTurns(stubGame);
+        teamIndex = 1;
+        // Team should be cat
+        expect(controller.getCurrentTeam(stubGame)).toEqual(stubGame.teams[teamIndex]);
+        expect(stubGame.teams[teamIndex].turn).toBe(true);
+        expect(stubGame.teams[0].turn).toBe(false);
+    });
 
-    // Start game
-    let startState = GameState.SETUP;
-    let nextState = GameState.ENTRY;
-    controller.gameStateMachine({}, stubGame, startState, {});
-    expect(stubGame.state).toBe(nextState);
+    describe('increment game state', () => {
+        function startGame() {
+            // Start game
+            controller.gameStateMachine({}, stubGame, GameState.SETUP, {});
+        }
+        test('initial state', () => {
+            // Init state
+            expect(stubGame.answer).toBeUndefined();
+            expect(stubGame.cachedPlayers).toEqual([]);
+            // check sum of scores is 0
+            let scores = stubGame.teams.reduce((acc, cur) => acc + cur.score, 0);
+            expect(scores).toBe(0);
+        });
 
-    // Hint
-    startState = GameState.ENTRY;
-    nextState = GameState.HINT;
-    const answer = "Cats";
-    controller.gameStateMachine({}, stubGame, startState, { answer: answer });
-    expect(stubGame.state).toBe(nextState);
-    // Check answer
-    expect(stubGame.question.answer).toBe(answer);
+        test('should start the game', () => {
+            startGame();
+            expect(stubGame.state).toBe(GameState.ENTRY);
+        });
 
-    // Steal
-    startState = GameState.HINT;
-    nextState = GameState.STEAL;
-    controller.gameStateMachine({}, stubGame, startState, {});
-    expect(stubGame.state).toBe(nextState);
+        test('should move to hint state', () => {
+            // Hint
+            let startState = GameState.ENTRY;
+            let nextState = GameState.HINT;
+            const answer = "Cats";
+            controller.gameStateMachine({}, stubGame, startState, { answer: answer });
+            expect(stubGame.state).toBe(nextState);
+            // Check answer
+            expect(stubGame.question.answer).toBe(answer);
+        });
 
-    // Guess
-    startState = GameState.STEAL;
-    nextState = GameState.GUESS;
-    controller.gameStateMachine({}, stubGame, startState, { correct: false });
-    expect(stubGame.state).toBe(nextState);
+        test('should allow steal first', () => {
+            // Steal
+            let startState = GameState.HINT;
+            let nextState = GameState.STEAL;
+            controller.gameStateMachine({}, stubGame, startState, {});
+            expect(stubGame.state).toBe(nextState);
+        });
 
-    // check sum of scores is 0
-    scores = stubGame.teams.reduce((acc, cur) => acc + cur.score, 0);
-    expect(scores).toBe(0);
+        test('should allow guess on missed steal', () => {
+            startGame();
+            // Guess
+            let startState = GameState.STEAL;
+            let nextState = GameState.GUESS;
+            let scores = stubGame.teams.reduce((acc, cur) => acc + cur.score, 0);
+            controller.gameStateMachine({}, stubGame, startState, { correct: false });
+            expect(stubGame.state).toBe(nextState);
 
-    // Reveal
-    startState = GameState.STEAL;
-    nextState = GameState.REVEAL;
-    controller.gameStateMachine({}, stubGame, startState, { correct: true });
-    expect(stubGame.state).toBe(nextState);
+            // check sum of scores is not changed
+            expect(stubGame.teams.reduce((acc, cur) => acc + cur.score, 0)).toBe(scores);
 
-    // check sum of scores is 1
-    scores = stubGame.teams.reduce((acc, cur) => acc + cur.score, 0);
-    expect(scores).toBe(1);
+        });
 
-    startState = GameState.GUESS;
-    nextState = GameState.REVEAL;
-    controller.gameStateMachine({}, stubGame, startState, { correct: true });
-    expect(stubGame.state).toBe(nextState);
+        test('should reveal answer on successful steal', () => {
+            startGame();
+            // Reveal
+            let startState = GameState.STEAL;
+            let nextState = GameState.REVEAL;
+            let scores = stubGame.teams.reduce((acc, cur) => acc + cur.score, 0);
+            controller.gameStateMachine({}, stubGame, startState, { correct: true });
+            expect(stubGame.state).toBe(nextState);
 
-    // check sum of scores is 2
-    scores = stubGame.teams.reduce((acc, cur) => acc + cur.score, 0);
-    expect(scores).toBe(2);
+            // check sum of scores is 1 more
+            expect(stubGame.teams.reduce((acc, cur) => acc + cur.score, 0)).toBe(scores + 1);
+        });
 
-    startState = GameState.GUESS;
-    nextState = GameState.REVEAL;
-    controller.gameStateMachine({}, stubGame, startState, { correct: false });
-    expect(stubGame.state).toBe(nextState);
+        test('should properly increment score on successful guess', () => {
+            startGame();
+            let startState = GameState.GUESS;
+            let nextState = GameState.REVEAL;
+            let scores = stubGame.teams.reduce((acc, cur) => acc + cur.score, 0);
+            controller.gameStateMachine({}, stubGame, startState, { correct: true });
+            expect(stubGame.state).toBe(nextState);
 
-    // check sum of scores is 2
-    scores = stubGame.teams.reduce((acc, cur) => acc + cur.score, 0);
-    expect(scores).toBe(2);
+            // check sum of scores is 1 more
+            expect(stubGame.teams.reduce((acc, cur) => acc + cur.score, 0)).toBe(scores + 1);
+        });
 
-    // ENTRY
-    startState = GameState.REVEAL;
-    nextState = GameState.ENTRY;
-    controller.gameStateMachine({}, stubGame, startState, {});
-    expect(stubGame.state).toBe(nextState);
+        test('should reveal answer once all guesses are complete', () => {
+            startGame();
+            let startState = GameState.GUESS;
+            let nextState = GameState.REVEAL;
+            let scores = stubGame.teams.reduce((acc, cur) => acc + cur.score, 0);
+            controller.gameStateMachine({}, stubGame, startState, { correct: false });
+            expect(stubGame.state).toBe(nextState);
 
-    // END
-    startState = GameState.END;
-    nextState = GameState.SETUP;
-    controller.gameStateMachine({}, stubGame, startState, {});
-    expect(stubGame.state).toBe(nextState);
-    // check no cached players
-    expect(stubGame.cachedPlayers).toEqual([]);
-    // check sum of scores is 0
-    scores = stubGame.teams.reduce((acc, cur) => acc + cur.score, 0);
-    expect(scores).toBe(0);
-    // check no turn set
-    let turn = stubGame.teams.reduce((acc, cur) => acc || cur.turn, false);
-    expect(turn).toBe(false);
+            // check sum of scores is not changed
+            expect(stubGame.teams.reduce((acc, cur) => acc + cur.score, 0)).toBe(scores);
+        });
+
+        test('should continue game', () => {
+            // ENTRY
+            let startState = GameState.REVEAL;
+            let nextState = GameState.ENTRY;
+            controller.gameStateMachine({}, stubGame, startState, {});
+            expect(stubGame.state).toBe(nextState);
+        });
+
+        test('should be able to end game', () => {
+            // END
+            let startState = GameState.END;
+            let nextState = GameState.SETUP;
+            controller.gameStateMachine({}, stubGame, startState, {});
+            expect(stubGame.state).toBe(nextState);
+            // check no cached players
+            expect(stubGame.cachedPlayers).toEqual([]);
+            // check sum of scores is 0
+            let scores = stubGame.teams.reduce((acc, cur) => acc + cur.score, 0);
+            expect(scores).toBe(0);
+            // check no turn set
+            let turn = stubGame.teams.reduce((acc, cur) => acc || cur.turn, false);
+            expect(turn).toBe(false);
+        });
+    });
 });
--- utils/__tests__/triviaController.test.js
+++ utils/__tests__/triviaController.test.js
@@ -1,6 +1,6 @@
 import { TriviaController } from '../triviaController';
 import { GameState } from '../enums';
-import { bob, stubGame } from '../__stubs__/gameStub';
+import { bob, sam, stubGame } from '../__stubs__/gameStub';
 import rawData from "../__data__/trivia.json";
 
 jest.mock('../emitter');
@@ -20,55 +20,172 @@ describe("Trivia Controller Test", () => {
         expect(stubGame.state).toBe(GameState.SETUP);
     });
 
-    test('increment game state', () => {
-        // Init state
-        expect(stubGame.answer).toBeUndefined();
-        expect(stubGame.cachedPlayers).toEqual([]);
-        // check sum of scores is 0
-        let scores = stubGame.teams.reduce((acc, cur) => acc + cur.score, 0);
-        expect(scores).toBe(0);
+    describe('increment game state', () => {
 
-        // Can't start an empty game
-        let startState = GameState.SETUP;
-        let nextState = GameState.SETUP;
-        controller.gameStateMachine({}, stubGame, startState, {});
-        expect(stubGame.state).toBe(nextState);
+        function loadQuestions() {
+            // Load data
+            controller.loadData({}, stubGame.id, input);
+        }
 
-        // Load data
-        controller.loadData({}, stubGame.id, input);
-        startState = GameState.SETUP;
-        nextState = GameState.ENTRY;
-        let questionCount = controller.questions.length;
-        controller.gameStateMachine({}, stubGame, startState, {});
-        expect(stubGame.state).toBe(nextState);
-        expect(controller.questions.length).toBe(questionCount - 1);
+        test('start trivia game', () => {
+            // Init state
+            expect(stubGame.answer).toBeUndefined();
+            expect(stubGame.cachedPlayers).toEqual([]);
+            // check sum of scores is 0
+            let scores = stubGame.teams.reduce((acc, cur) => acc + cur.score, 0);
+            expect(scores).toBe(0);
+        });
 
-        // Hint
-        startState = GameState.ENTRY;
-        nextState = GameState.HINT;
-        const answer = stubGame.question.answer;
-        controller.gameStateMachine({}, stubGame, startState);
-        expect(stubGame.state).toBe(nextState);
-        // Check answer
-        expect(stubGame.question.answer).toBe(answer);
+        test('should not start an empty game', () => {
+            // Can't start an empty game
+            let startState = GameState.SETUP;
+            let nextState = GameState.SETUP;
+            controller.gameStateMachine({}, stubGame, startState, {});
+            expect(stubGame.state).toBe(nextState);
+        })
 
-        // Buzz In
-        startState = GameState.HINT;
-        nextState = GameState.GUESS;
-        let team = stubGame.teams.find(team => team.id === bob.teamID);
-        expect(team.players.find(player => player.id === bob.id).turn).toBe(false);
-        expect(team.turn).toBe(false);
-        controller.gameStateMachine({}, stubGame, startState, { player: bob });
-        team = stubGame.teams.find(team => team.id === bob.teamID);
-        expect(stubGame.state).toBe(nextState);
-        expect(team.players.find(player => player.id === bob.id).turn).toBe(true);
-        expect(team.turn).toBe(true);
+        test('should start a game and load a question', () => {
+            // Load data
+            loadQuestions();
+            let startState = GameState.SETUP;
+            let nextState = GameState.ENTRY;
+            let questionCount = controller.questions.length;
+            expect(stubGame.question).toBeNull();
+            controller.gameStateMachine({}, stubGame, startState, {});
+            expect(stubGame.state).toBe(nextState);
+            expect(controller.questions.length).toBe(questionCount - 1);
+            expect(stubGame.question).toBeDefined();
+        });
 
-        // Skip
-        startState = GameState.HINT;
-        nextState = GameState.REVEAL;
-        controller.gameStateMachine({}, stubGame, startState);
-        expect(stubGame.state).toBe(nextState);
+        test('should move into hint state', () => {
+            // Hint
+            loadQuestions();
+            let startState = GameState.ENTRY;
+            let nextState = GameState.HINT;
+            const answer = stubGame.question.answer;
+            controller.gameStateMachine({}, stubGame, startState);
+            expect(stubGame.state).toBe(nextState);
+            // Check answer
+            expect(stubGame.question.answer).toBe(answer);
+            expect(stubGame.question.answer).toBeDefined();
+        });
+
+        test('should allow player to buzz in', () => {
+            // Buzz In
+            loadQuestions();
+            let startState = GameState.HINT;
+            let nextState = GameState.GUESS;
+            let team = stubGame.teams.find(team => team.id === bob.teamID);
+            expect(team.players.find(player => player.id === bob.id).turn).toBe(false);
+            expect(team.turn).toBe(false);
+            controller.gameStateMachine({}, stubGame, startState, { player: bob });
+            team = stubGame.teams.find(team => team.id === bob.teamID);
+            expect(stubGame.state).toBe(nextState);
+            expect(team.players.find(player => player.id === bob.id).turn).toBe(true);
+            expect(team.turn).toBe(true);
+        });
+
+        test('should allow game to skip answering', () => {
+            // Skip
+            loadQuestions();
+            let startState = GameState.HINT;
+            let nextState = GameState.REVEAL;
+            controller.gameStateMachine({}, stubGame, startState);
+            expect(stubGame.state).toBe(nextState);
+        });
+
+        test('should properly deduct points on wrong guess', () => {
+            // Guess Wrong
+            loadQuestions();
+            let startState = GameState.HINT;
+            let nextState = GameState.GUESS;
+            controller.gameStateMachine({}, stubGame, startState, { player: bob });
+            let team = stubGame.teams.find(team => team.id === bob.teamID);
+            let score = team.score;
+            let delta = stubGame.question.points;
+            startState = GameState.GUESS;
+            nextState = GameState.STEAL;
+            controller.gameStateMachine({}, stubGame, startState, { correct: false });
+            team = stubGame.teams.find(team => team.id === bob.teamID);
+            expect(stubGame.state).toBe(nextState);
+            expect(team.score).toBe(score - delta);
+            expect(team.turn).toBe(false);
+        });
+
+        test('should properly add points on correct guess', () => {
+            // Guess Correct
+            loadQuestions();
+            let startState = GameState.HINT;
+            let nextState = GameState.GUESS;
+            controller.gameStateMachine({}, stubGame, startState, { player: bob });
+            let team = stubGame.teams.find(team => team.id === bob.teamID);
+            let score = team.score;
+            let delta = stubGame.question.points;
+            startState = GameState.GUESS;
+            nextState = GameState.REVEAL;
+            controller.gameStateMachine({}, stubGame, startState, { correct: true });
+            team = stubGame.teams.find(team => team.id === bob.teamID);
+            expect(stubGame.state).toBe(nextState);
+            expect(team.score).toBe(score + delta);
+            expect(team.turn).toBe(true);
+        });
+
+        test('should properly add points on correct steal', () => {
+            // Guess Correct
+            loadQuestions();
+            let startState = GameState.HINT;
+            let nextState = GameState.GUESS;
+            controller.gameStateMachine({}, stubGame, startState, { player: bob });
+            startState = GameState.GUESS;
+            nextState = GameState.STEAL;
+            controller.gameStateMachine({}, stubGame, startState, { correct: false });
+
+            startState = GameState.STEAL;
+            nextState = GameState.REVEAL;
+            let team = stubGame.teams.find(team => team.id === sam.teamID);
+            expect(team.players.reduce((acc, player) => acc || player.turn, false)).toBe(false);
+            let score = team.score;
+            let delta = stubGame.question.points;
+            controller.gameStateMachine({}, stubGame, startState, { correct: true });
+            team = stubGame.teams.find(team => team.id === sam.teamID);
+            expect(stubGame.state).toBe(nextState);
+            expect(team.score).toBe(score + delta);
+            expect(team.turn).toBe(true);
+        });
+
+        test('should reveal answer and get next question', () => {
+            loadQuestions();
+            let startState = GameState.REVEAL;
+            let nextState = GameState.ENTRY;
+            let question = stubGame.question;
+            controller.gameStateMachine({}, stubGame, startState);
+            expect(stubGame.teams.reduce((acc, team) => acc || team.turn, false)).toBe(false);
+            expect(stubGame.state).toBe(nextState);
+            expect(stubGame.question).not.toBe(question);
+        });
+
+        test('should end the game', () => {
+            let startState = GameState.END;
+            let nextState = GameState.SETUP;
+            controller.gameStateMachine({}, stubGame, startState);
+            expect(stubGame.teams.reduce((acc, team) => acc || team.turn, false)).toBe(false);
+            expect(stubGame.state).toBe(nextState);
+            let scores = stubGame.teams.reduce((acc, cur) => acc + cur.score, 0);
+            expect(scores).toBe(0);
+        });
+
+        test('should end the game if no questions remain', () => {
+            loadQuestions();
+            controller.questions = [];
+            controller.category = [];
+            let startState = GameState.REVEAL;
+            let nextState = GameState.SETUP;
+            controller.gameStateMachine({}, stubGame, startState);
+            expect(stubGame.teams.reduce((acc, team) => acc || team.turn, false)).toBe(false);
+            expect(stubGame.state).toBe(nextState);
+            let scores = stubGame.teams.reduce((acc, cur) => acc + cur.score, 0);
+            expect(scores).toBe(0);
+        });
     });
 
     test('load data', () => {
